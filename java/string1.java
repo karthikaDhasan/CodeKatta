a6a363f3a7c4406f4c121ec396d29a7225da3fc2
karthikaDhasan <36623865+karthikaDhasan@users.noreply.github.com>
2018-06-26 15:29:09 +0530

    Create string1.java

---
+++ java/string1.java
@@ -0,0 +1,45 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		String st=sc.nextLine();
+		String[] sp=st.split(" ");
+		String s2;
+		int i;
+		System.out.println("after split:");
+		
+		for(String  s1:sp)
+		{          
+			System.out.print(" " +s1);
+			System.out.println("\n");
+			s2=s1;
+		}
+		for(String s1:sp)
+		{
+		 for(i=0;i<s1.length();i++)
+		{
+		char a[]=s1.toCharArray();
+		 
+		   if(Character.isUpperCase(a[i]))
+		  {
+		      	System.out.print(Character.toLowerCase(a[i]));
+		  }
+		  else
+		  {
+		  	System.out.print(Character.toUpperCase(a[i]) );
+		  }
+		}
+	}
+	
+	}
+	
+}
