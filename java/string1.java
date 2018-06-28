4fae23d3243e4d8cd9edaf9357d9f47872cff730
karthikaDhasan <36623865+karthikaDhasan@users.noreply.github.com>
2018-06-28 15:11:25 +0530

    Update string1.java

--- java/string1.java
+++ java/string1.java
@@ -1,55 +1,86 @@
-/* package whatever; // don't place package name! */
+import java.util.Scanner;
 
-import java.util.*;
-import java.lang.*;
-import java.io.*;
 
-/* Name of the class has to be "Main" only if the class is public. */
-class Ideone
-{
-	public static void main (String[] args) throws java.lang.Exception
-	{
-		// your code goes here
-		Scanner sc=new Scanner(System.in);
-		String st=sc.nextLine();
-		String[] sp=st.split(" ");
-		String s2="";
-		int i;
-		System.out.println("after split:");
+public class ddd {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 		
-		for(String  s1:sp)
-		{          
-			System.out.print(" " +s1);
+				int i;
+				System.out.println("enter the string:");
+				Scanner sc=new Scanner(System.in);
+				String st=sc.nextLine();
+				
+				String[] sp=st.split(" ");
+				
+					for(String s1:sp)
+					{
+						StringBuilder sb=new StringBuilder(s1);
+						System.out.print(" " +sb.reverse().toString());
+					}
+					System.out.println(" ");
+					for(String s1:sp)
+					{
+					 StringBuilder sb=new StringBuilder(s1);
+					 
+					 String b=new String(sb.reverse() );
+					 for(i=0;i<b.length();i++)
+					      { 
+					
+					char a[]=b.toCharArray();
+					char a1[]=s1.toCharArray();
+					 
+					   if(Character.isUpperCase(a1[i]))
+					  {
+					     System.out.print(Character.toUpperCase(a[i]));
+					      	
+					      	
+					  }
+					  else
+					  {
+					  	System.out.print(Character.toLowerCase(a[i]) );
+					  }
+					   
+					}
+				}
 			
-			s2=s1;
+					System.out.println(" ");
+					for(String s1:sp)
+					{
+					 StringBuilder sb=new StringBuilder(s1);
+					 
+					 String b=new String(" " +sb.reverse());
+					 for(i=0;i<b.length();i++)
+					      { 
+					
+					char a[]=b.toCharArray();
+					
+					 
+					   if(Character.isUpperCase(a[i]))
+					  {
+					     System.out.print(Character.toLowerCase(a[i]));
+					      	
+					      	
+					  }
+					  else
+					  {
+					  	System.out.print(Character.toUpperCase(a[i]) );
+					  }
+					 
+					}
+				}
+			
+				
+			}
+
 		}
-		System.out.println(" ");
-		System.out.println("the reverse:"); 
-		for(String s1:sp)
-		{
-		 StringBuilder sb=new StringBuilder(s1);
-		 System.out.print(sb.reverse());
-		 String b=new String(sb);
-		 for(i=0;i<b.length();i++)
-		      { 
-		
-		char a[]=b.toCharArray();
-		 
-		   if(Character.isUpperCase(a[i]))
-		  {
-		     System.out.print(Character.toLowerCase(a[i]));
-		      	
-		      	
-		  }
-		  else
-		  {
-		  	System.out.print(Character.toUpperCase(a[i]) );
-		  }
-		}
-	}
-		
-		
+OUTPUT:
+enter the string:
+Wipro Technologies Bangalore
+ orpiW seigolonhceT erolagnaB 
+OrpiwSeigolonhcetErolagnab 
+ ORPIw SEIGOLONHCEt EROLAGNAb
 	
-	}
-	
-}
