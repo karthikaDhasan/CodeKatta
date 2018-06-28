03dd554d6e4f8612e1a67429d92fb7fbf28e9398
karthikaDhasan <36623865+karthikaDhasan@users.noreply.github.com>
2018-06-28 14:17:50 +0530

    Update numbersum.java

--- java/numbersum.java
+++ java/numbersum.java
@@ -10,19 +10,27 @@ class Ideone
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
-		int  s=-123,sum=0,t=0,sum1=0;
+		int  s=-628567,sum=0,t=0,sum1=0;
 		int n=Math.abs(s);
-	  while(n>0)
+	             while(n>0)
 		{
 			t=n%10;
 			sum=sum+t;
 			n=n/10;
-			if(sum>9)
+		}	
+		if(sum>9)
 			{
 				n=sum;
 				sum=0;
+				 while(n>0)
+		{
+			t=n%10;
+			sum=sum+t;
+			n=n/10;
+		}	
+				
 			}
-		}
+		
 		sum1=sum;
 		if(s<0)
 		{
