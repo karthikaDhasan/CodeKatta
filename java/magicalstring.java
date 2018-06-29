a0c9b43a601e40f5f5998b95d2823fb11e547e67
karthikaDhasan <36623865+karthikaDhasan@users.noreply.github.com>
2018-06-29 15:00:47 +0530

    Create magicalstring.java

---
+++ java/magicalstring.java
@@ -0,0 +1,81 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner sc=new Scanner(System.in);
+	
+	String st=sc.nextLine();
+	System.out.println(st);
+	
+	for(int i=0;i<st.length();i++)
+	{
+		char ch=st.charAt(i);
+		if(ch=='a')
+		 ch='z';
+		else if(ch=='b')
+		 ch='y';
+		else if(ch=='c')
+		 ch='x';
+	            else if(ch=='d')
+		 ch='w';
+		 else if(ch=='e')
+		 ch='v';
+		 else if(ch=='f')
+		 ch='u';
+		 else if(ch=='g')
+		 ch='t';
+		 else if(ch=='h')
+		 ch='s';
+		 else if(ch=='i')
+		 ch='r';
+		 else if(ch=='j')
+		 ch='q';
+		 else if(ch=='k')
+		 ch='p';
+		 else if(ch=='l')
+		 ch='o';
+		 else if(ch=='m')
+		 ch='n';
+		 else if(ch=='z')
+		 ch='a';
+		 else if(ch=='y')
+		 ch='b';
+		 else if(ch=='x')
+		 ch='c';
+		 else if(ch=='w')
+		 ch='d';
+		 else if(ch=='v')
+		 ch='e';
+		 else if(ch=='u')
+		 ch='f';
+		 else if(ch=='t')
+		 ch='g';
+		 else if(ch=='s')
+		 ch='h';
+		 else if(ch=='r')
+		 ch='i';
+		 else if(ch=='q')
+		 ch='j';
+		 else if(ch=='p')
+		 ch='k';
+		 else if(ch=='o')
+		 ch='l';
+		 else if(ch=='n')
+		 ch='m';
+		 
+		 System.out.print(ch);
+	}
+	}
+}
+
+OUTPUT:
+varun
+ANSWER:
+ezifm
